add86304f5a0bc3ceced5f6347d28209509f2875
Addy <adnan.khakoo@hotmail.co.uk>
2019-11-12 19:22:28 -0800

    chore:Adding Bikeshare code

---
+++ bikeshare.py
@@ -0,0 +1,35 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+
+    while True:
+
+city = input("Would you like to see data for Chicago, New York City or Washington").lower()
+
+try:
+
+if city not in CITY_DATA:
+
+print("Sorry, {} is not a valid city. Please type again by entering either 'Chicago', 'New York City' OR 'Washington' again".format(city))
+
+continue
+
+else:
+
+break
